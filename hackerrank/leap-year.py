f0df7f83c7b2a227aaae80be0c23ae0bc3cb66a3
Harshith A K <harshithak5520@gmail.com>
2026-01-13 22:51:53 +0530

    Update leap-year.py
    solved leap year problem with some comments

--- hackerrank/leap-year.py
+++ hackerrank/leap-year.py
@@ -1,6 +1,8 @@
 def is_leap(year):
   leap=False
+  #applying condition for checking given year is leap year or not
   if(year%400==0) or (year%4==0 and year%100!=0):
+    # if the year is leap returns true
     returnTrue
   else:
     return False
