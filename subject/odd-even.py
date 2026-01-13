a6b2f5f95116e7153795354f628d0e6ae8656ccd
Harshith A K <harshithak5520@gmail.com>
2026-01-13 23:08:08 +0530

    Create odd-even.py
    solved a problem of odd-even number

---
+++ subject/odd-even.py
@@ -0,0 +1,7 @@
+n=int(input("enter a num:"))
+print("the enterd number is: ",n)
+if n%2==0:
+  print("the given number is even")
+else:
+  print("odd number")
+  # if the number is even priint even number otherwise printing odd number
