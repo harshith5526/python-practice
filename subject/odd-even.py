6d58450f700dd654f6b9443a7ca6eecf4bc24678
Harshith A K <harshithak5520@gmail.com>
2026-01-14 22:38:05 +0530

    Update odd-even.py
    checking the given number is odd or even number

--- subject/odd-even.py
+++ subject/odd-even.py
@@ -1,7 +1,9 @@
 n=int(input("enter a num:"))
 print("the enterd number is: ",n)
 if n%2==0:
+  #  if the number is even print even number
   print("the given number is even")
 else:
+    #  otherwise printing odd number
   print("odd number")
-  # if the number is even priint even number otherwise printing odd number
+
